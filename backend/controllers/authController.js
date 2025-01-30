869d2dd9c6eba1e7d9aa7265c21b79b64e4e4b91
sanjay212003 <139911764+sanjay212003@users.noreply.github.com>
2025-01-30 19:19:52 +0530

    removed unwanted declarations and files

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,4 @@
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import sendResponse from '../utils/responseHandler.js';
 import generateToken from '../utils/generateToken.js';
