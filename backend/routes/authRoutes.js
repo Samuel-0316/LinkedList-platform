18cbf0f34298d9052bf1d64889ad9e51a78363f3
02Shivaraj <shivaraj.a.2611@gmail.com>
2025-01-30 20:26:02 +0530

    updated backend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import { signup, login, updateProfile} from '../controllers/authController.js';
-import { protect } from '../middlewares/authMiddleware.js';
+import { signup, login} from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.put('/update', protect, updateProfile);
 
 export default router;
